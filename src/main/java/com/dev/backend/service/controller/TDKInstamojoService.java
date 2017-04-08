ea4c8e4dbc1c3ecde8587749e1464358d2f98ae9
ashus <ashus@ashus-w02.vmware.com>
2017-04-09 01:47:44 +0530

    Added test URL changes

--- src/main/java/com/dev/backend/service/controller/TDKInstamojoService.java
+++ src/main/java/com/dev/backend/service/controller/TDKInstamojoService.java
@@ -29,36 +29,48 @@ public class TDKInstamojoService {
 
 	private static String javaClientId = "7xqNuJKyk6ackBvxZdqv7NbBMbSokQQoYJGXCI6k";// "QmzoZUaVetzKTuMcOdGw4S7LgZxwRuDihqzT3BW0";
 
+	private static String testClientId = "QmzoZUaVetzKTuMcOdGw4S7LgZxwRuDihqzT3BW0";
+
+	private static String testJavaSecret = "suBJDVFXQTVxZ2C7FgMqzIneuJFoU34abRYWh"
+			+ "9HeF2nJhhK62LgCiyaCRxqSueusQSX0NyD5UYstyI2gj7G5gr5bQ9DjDvUKVPeCuYkd7" + "e1A6UEUNEVjUgOwrDLxY06a";
+
 	private static InstamojoAccessTokenTO accessTokens;
 
 	private static String javaClientSecret = "dQ9tPZnqt6VsCBecVOvkoa63chKy39H6FdUVQ"
-			+ "fC7wryd95hOUx8dEA6JXNtZh0cFg5QLgy71ZZqoHdzoa8s4lsc4NXfYN4fYsfZJ68uVq"
-			+ "MiK79dGAJvjY7pcpzLfVMzD";/*
-									 * "suBJDVFXQTVxZ2C7FgMqzIneuJFoU34abRYWh" +
-									 * "9HeF2nJhhK62LgCiyaCRxqSueusQSX0NyD5UYstyI2gj7G5gr5bQ9DjDvUKVPeCuYkd7"
-									 * + "e1A6UEUNEVjUgOwrDLxY06a";
-									 */
+			+ "fC7wryd95hOUx8dEA6JXNtZh0cFg5QLgy71ZZqoHdzoa8s4lsc4NXfYN4fYsfZJ68uVq" + "MiK79dGAJvjY7pcpzLfVMzD";
 
 	private static String clientId = "euaipxFmcumKSPHpA6DhDagcizEHhaVgvpKwqW2";
 
 	private static String clientSercret = "yXalaUv1v5EZFKPIoGqRr6y3dYziwVSFWXVQp"
 			+ "PgflVedBLZEbmq0gpEiGO9riFDW1LroDZqjhJzYi9PsxIWqGHVdHsQVJNB1" + "hEOnWbAq562ieVxVJ3anTvlmvgDUHped";
-	
+
 	private static Date tokenExpiryTime = new Date(Instant.now().toEpochMilli());
-	
-	private static long expiryMiliSec = 36000*1000;
-			
+
+	private static long expiryMiliSec = 36000 * 1000;
+
 	private static ObjectMapper mapper = new ObjectMapper();
 
 	public static void generateAccessTokens() throws JsonParseException, JsonMappingException, IOException {
+		
+		boolean isTest = Boolean.parseBoolean(System.getProperty("isTest"));
+		
 		Map<String, String> requestMap = new HashMap<String, String>();
-		requestMap.put(InstamojoConstants.CLIENT_ID_KEY, javaClientId);
-		requestMap.put(InstamojoConstants.CLIENT_SALT_KEY, javaClientSecret);
 		requestMap.put(InstamojoConstants.GRANT_TYPE_KEY, InstamojoConstants.GRANT_TYPE_VALUE);
-		String response = HttpUtil.sendPostRequest(InstamojoConstants.test_token_URL, null, requestMap);
+		String tokenUrl = InstamojoConstants.test_token_URL;
+		
+		if(isTest) {
+			requestMap.put(InstamojoConstants.CLIENT_ID_KEY, testClientId);
+			requestMap.put(InstamojoConstants.CLIENT_SALT_KEY, testJavaSecret);
+			tokenUrl = "https://test.instamojo.com/oauth2/token/";
+		} else {
+			requestMap.put(InstamojoConstants.CLIENT_ID_KEY, javaClientId);
+			requestMap.put(InstamojoConstants.CLIENT_SALT_KEY, javaClientSecret);
+		}
+		
+		String response = HttpUtil.sendPostRequest(tokenUrl, null, requestMap);
 		System.out.println(response);
 		accessTokens = mapper.readValue(response, InstamojoAccessTokenTO.class);
-		tokenExpiryTime = new Date(Instant.now().toEpochMilli()+expiryMiliSec);
+		tokenExpiryTime = new Date(Instant.now().toEpochMilli() + expiryMiliSec);
 		System.out.println(tokenExpiryTime);
 	}
 
@@ -72,19 +84,18 @@ public class TDKInstamojoService {
 			}
 		}
 		Map<String, String> headers = new HashMap<>();
-        headers.put("Authorization", accessTokens.getTokenType() + " " + accessTokens.getAccessToken());
-        Map<String, String> params = new HashMap<>();
+		headers.put("Authorization", accessTokens.getTokenType() + " " + accessTokens.getAccessToken());
+		Map<String, String> params = new HashMap<>();
 
-        params.put("name", paymentRequest.getBuyer_name());
-        params.put("email", paymentRequest.getEmail());
-        params.put("phone", paymentRequest.getPhone());
-        params.put("purpose", paymentRequest.getPurpose());
-        params.put("amount", String.valueOf(paymentRequest.getAmount()));
-        params.put("redirect_url", "http://thekadesikhana.com/resources/views/thankYou.html");
+		params.put("name", paymentRequest.getBuyer_name());
+		params.put("email", paymentRequest.getEmail());
+		params.put("phone", paymentRequest.getPhone());
+		params.put("purpose", paymentRequest.getPurpose());
+		params.put("amount", String.valueOf(paymentRequest.getAmount()));
+		params.put("redirect_url", "http://thekadesikhana.com/resources/views/thankYou.html");
 
-            String response = HttpUtil.sendPostRequest(InstamojoConstants.PAYMENT_API_URL, headers,
-                    params);
-            System.out.println(response);
+		String response = HttpUtil.sendPostRequest(InstamojoConstants.PAYMENT_API_URL, headers, params);
+		System.out.println(response);
 		InstamojoPaymentResponseTO responseObject = mapper.readValue(response, InstamojoPaymentResponseTO.class);
 		return responseObject;
 	}
