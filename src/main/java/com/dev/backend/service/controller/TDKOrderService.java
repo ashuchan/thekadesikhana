37b7409aaef2661ae4416e1c3453666b6c8c9056
ashus <ashus@ashus-w02.vmware.com>
2017-04-08 21:47:29 +0530

    New Payment URL

--- src/main/java/com/dev/backend/service/controller/TDKOrderService.java
+++ src/main/java/com/dev/backend/service/controller/TDKOrderService.java
@@ -8,6 +8,8 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +31,7 @@ import com.dev.backend.to.InstamojoPaymentResponseTO;
 import com.dev.backend.to.InstamojoPaymentTO;
 import com.dev.backend.to.OrderTransactionTO;
 import com.dev.backend.to.OrderTransactionTO.MenuItem;
+import com.dev.backend.to.WebOrderTO.UserInfo;
 import com.dev.backend.to.PaymentGatewayTO;
 import com.dev.backend.to.WebOrderTO;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -42,69 +45,66 @@ public class TDKOrderService extends TDKServices {
 	public String nextSessionId() {
 		return new BigInteger(130, random).toString(32);
 	}
-	
-	@RequestMapping(value="/order/{orderId}",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(value = "/order/{orderId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Order getOrder(@PathVariable String orderId) {
 		return delegate.getOrder(orderId);
 	}
-	
-	@RequestMapping(value="/order",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(value = "/order", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Order getOrderByParam(@RequestParam String orderId) {
 		return delegate.getOrder(orderId);
 	}
-	
-	@RequestMapping(value="/order/user/{userId}",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(value = "/order/user/{userId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody List<Order> getOrdersByCustomer(@PathVariable String userId) {
 		return delegate.getOrdersByCustomer(userId);
 	}
-	
-	@RequestMapping(value="/order/user",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(value = "/order/user", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody List<Order> getOrdersByCustomerParam(@RequestParam String userId) {
 		return delegate.getOrdersByCustomer(userId);
 	}
-	
-	@RequestMapping(value="/order", method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody PaymentGatewayTO createOrder(@RequestBody OrderTransactionTO order) throws JsonParseException, JsonMappingException, IOException {
+
+	@RequestMapping(value = "/order", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody PaymentGatewayTO createOrder(@RequestBody OrderTransactionTO order)
+			throws JsonParseException, JsonMappingException, IOException {
 		/*
-		 * Create new payment with instamojo
-		 * Create new order
-		 * Generate new Order with order id from instamojo
-		 * create transaction with payment id
-		 * Update Database
-		 * return object with payment details
+		 * Create new payment with instamojo Create new order Generate new Order
+		 * with order id from instamojo create transaction with payment id
+		 * Update Database return object with payment details
 		 */
 		User user = delegate.getUser(order.getUserId());
 		UserAddress address = delegate.getAddressById(order.getAddressId());
 		List<OrderItem> menuItems = new ArrayList<OrderItem>();
-		order.getMenuItems().forEach(
-				m -> menuItems.add(new OrderItem(delegate.getMenuItemById(m
-						.getMenuItem()), m.getQuantity())));
+		order.getMenuItems()
+				.forEach(m -> menuItems.add(new OrderItem(delegate.getMenuItemById(m.getMenuItem()), m.getQuantity())));
 		int totalPrice = 0;
-		for(OrderItem m:menuItems) {
-			totalPrice+=m.getMenu().getPrice()*m.getQuantity();
+		for (OrderItem m : menuItems) {
+			totalPrice += m.getMenu().getPrice() * m.getQuantity();
 		}
-		boolean isCod = order.getIsCOD().startsWith("f")?false:true;
+		boolean isCod = order.getIsCOD().startsWith("f") ? false : true;
 
-		int gatewayPrice = totalPrice
-				- (order.getPromotionalWalletCut() + order.getWalletCashCut());
+		int gatewayPrice = totalPrice - (order.getPromotionalWalletCut() + order.getWalletCashCut());
 		String transactionId = null;
 		String orderId = null;
 		String paymentURL = null;
 		if (gatewayPrice > 0 && !isCod) {
 			InstamojoPaymentTO paymentRequest = null;
-			if(user.getPhone().equals("7348815961")) {
-				paymentRequest = new InstamojoPaymentTO(""+gatewayPrice, address.getName(), user.getEmail(), address.getMobileNumber());
+			if (user.getPhone().equals("7348815961")) {
+				paymentRequest = new InstamojoPaymentTO("" + gatewayPrice, address.getName(), user.getEmail(),
+						address.getMobileNumber());
 			} else {
-				paymentRequest = new InstamojoPaymentTO(""+gatewayPrice, user.getName(), user.getEmail(), user.getPhone());
+				paymentRequest = new InstamojoPaymentTO("" + gatewayPrice, user.getName(), user.getEmail(),
+						user.getPhone());
 			}
 			InstamojoPaymentResponseTO response = TDKInstamojoService.createPayment(paymentRequest);
 			transactionId = response.getId();
 			paymentURL = response.getLongurl();
-		} 
-		
+		}
+
 		orderId = nextSessionId();
-		
-		
+
 		Order orderObj = new Order();
 		orderObj.setAddress(address);
 		orderObj.setOrderDate(new Date(Instant.now().toEpochMilli()));
@@ -112,79 +112,133 @@ public class TDKOrderService extends TDKServices {
 		orderObj.setTotalPrice(totalPrice);
 		orderObj.setUser(user.getPhone());
 		orderObj.setStatus(OrderStatus.ACCEPTED);
-		menuItems.forEach(m->m.setOrder(orderObj));
+		menuItems.forEach(m -> m.setOrder(orderObj));
 		orderObj.setOrderItems(menuItems);
 		List<Transaction> transactions = new ArrayList<Transaction>();
-		if(order.getPromotionalWalletCut()>0){
+		if (order.getPromotionalWalletCut() > 0) {
 			// create promotional wallet transaction
-			transactions.add(createTransaction(order.getPromotionalWalletCut(), orderObj, user.getPhone(), null, TransactionCategory.PROMOTIONAL));
+			transactions.add(createTransaction(order.getPromotionalWalletCut(), orderObj, user.getPhone(), null,
+					TransactionCategory.PROMOTIONAL));
 		}
-		if(order.getWalletCashCut()>0) {
+		if (order.getWalletCashCut() > 0) {
 			// create wallet cash cut
-			transactions.add(createTransaction(order.getWalletCashCut(), orderObj, user.getPhone(), null, TransactionCategory.WALLET));
+			transactions.add(createTransaction(order.getWalletCashCut(), orderObj, user.getPhone(), null,
+					TransactionCategory.WALLET));
 		}
-		if( gatewayPrice >0) {
-			transactions.add(createTransaction(gatewayPrice, orderObj, user.getPhone(), transactionId, TransactionCategory.GATEWAY));
+		if (gatewayPrice > 0) {
+			transactions.add(createTransaction(gatewayPrice, orderObj, user.getPhone(), transactionId,
+					TransactionCategory.GATEWAY));
 		}
-		
-		delegate.createOrderWithTransactions(orderObj,transactions);
+
+		delegate.createOrderWithTransactions(orderObj, transactions);
 		PaymentGatewayTO gatewayTO = new PaymentGatewayTO(orderId, paymentURL);
 		return gatewayTO;
 	}
-	
-	@RequestMapping(value="/weborder", method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody PaymentGatewayTO webOrder(@RequestBody WebOrderTO order) throws JsonParseException, JsonMappingException, IOException {
+
+	@RequestMapping(value = "/weborder", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody PaymentGatewayTO webOrder(@RequestBody WebOrderTO order)
+			throws JsonParseException, JsonMappingException, IOException {
 		OrderTransactionTO orderTo = new OrderTransactionTO();
-		orderTo.setUserId("7348815961"); //Sample Web Order User
-		
+		orderTo.setUserId("7348815961"); // Sample Web Order User
+
 		UserAddress address = new UserAddress();
 		address.setUserId("7348815961");
 		address.setAddressLine1(order.getUserInfo().getAddress());
 		address.setMobileNumber(order.getUserInfo().getContact());
 		address.setName(order.getUserInfo().getName());
 		delegate.createAddress(address);
-		
-		orderTo.setAddressId(""+delegate.getTotalAddress());
+
+		orderTo.setAddressId("" + delegate.getTotalAddress());
 		order.getOrders().forEach(o -> {
 			MenuItem mi = new MenuItem();
-			mi.setMenuItem(""+o.getSku());
+			mi.setMenuItem("" + o.getSku());
 			mi.setQuantity(o.getQuantity());
 			orderTo.getMenuItems().add(mi);
 		});
-		
-		if(order.getPaymentType().equals("online"))
+
+		if (order.getPaymentType().equals("online"))
 			orderTo.setIsCOD("f");
-		
+
 		return createOrder(orderTo);
 	}
 
-	public Transaction createTransaction(int totalPrice, Order orderObj,
-			String walletId, String transactionId, TransactionCategory category) {
+	@RequestMapping(value = "/weborder", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void webOrderNew(@RequestBody WebOrderTO order, HttpServletResponse response)
+			throws JsonParseException, JsonMappingException, IOException {
+		UserAddress address = new UserAddress();
+		address.setUserId("7348815961");
+		address.setAddressLine1(order.getUserInfo().getAddress());
+		address.setMobileNumber(order.getUserInfo().getContact());
+		address.setName(order.getUserInfo().getName());
+		delegate.createAddress(address);
+		OrderTransactionTO orderTo = new OrderTransactionTO();
+		orderTo.setUserId("7348815961"); // Sample Web Order User
+
+		orderTo.setAddressId("" + delegate.getTotalAddress());
+		order.getOrders().forEach(o -> {
+			MenuItem mi = new MenuItem();
+			mi.setMenuItem("" + o.getSku());
+			mi.setQuantity(o.getQuantity());
+			orderTo.getMenuItems().add(mi);
+		});
+
+		if (!order.getPaymentType().equals("online")) {
+			orderTo.setIsCOD("t");
+			createOrder(orderTo);
+			return;
+		}
+
+		UserInfo user = order.getUserInfo();
+		String transactionId = null;
+		String orderId = null;
+		String paymentURL = null;
+		orderId = nextSessionId();
+
+		Order orderObj = new Order();
+		orderObj.setAddress(address);
+		orderObj.setOrderDate(new Date(Instant.now().toEpochMilli()));
+		orderObj.setOrderId(orderId);
+		orderObj.setTotalPrice(order.getPrice());
+		orderObj.setUser(user.getContact());
+		orderObj.setStatus(OrderStatus.ACCEPTED);
+		List<Transaction> transactions = new ArrayList<Transaction>();
+		transactions.add(createTransaction(order.getPrice(), orderObj, user.getContact(), transactionId,
+				TransactionCategory.GATEWAY));
+		String PaymentUrl = "https://www.instamojo.com/nandkumar90/thekadesikhana?embed=form&data_email="
+				+ user.getMail() + " &data_name=" + user.getName() + "&data_amount=" + order.getPrice() + "&data_phone="
+				+ user.getContact() + "&data_Field_78053=" + user.getAddress() + "&data_Field_96764=" + orderId
+				+ "&data_readonly=data_amount&data_hidden=data_Field_96764&data_hidden=data_Field_78053";
+		System.out.println("Payment URL - " + PaymentUrl);
+		delegate.createOrderWithTransactions(orderObj, transactions);
+		response.sendRedirect(PaymentUrl);
+	}
+
+	public Transaction createTransaction(int totalPrice, Order orderObj, String walletId, String transactionId,
+			TransactionCategory category) {
 		Transaction transaction = new Transaction();
 		transaction.setAmount(totalPrice);
 		transaction.setOrder(orderObj.getOrderId());
 		transaction.setWallet(walletId);
-		transaction.setTransactionId((transactionId==null)?nextSessionId():transactionId);
+		transaction.setTransactionId((transactionId == null) ? nextSessionId() : transactionId);
 		transaction.setTransactionCategory(category);
 		transaction.setTransactionType(TransactionType.DEBIT);
 		return transaction;
 	}
-	
-	@RequestMapping(value="/order/status", method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(value = "/order/status", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody String updateOrderStatus(@RequestBody String orderId, @RequestBody String status) {
 		delegate.updateOrderStatus(orderId, status);
 		return status;
 	}
-	
+
 	public String cancelOrder(String orderId) {
-		
+
 		return null;
 	}
-	
-	public Order updateOrder (Order order) {
-		
+
+	public Order updateOrder(Order order) {
+
 		return order;
 	}
-	
-}
 
+}
